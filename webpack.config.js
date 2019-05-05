190b621be796d85a616204903bacc0c090e680f5
augrinn <augrinn@gmail.com>
2019-05-05 12:30:55 +0300

    fix webpack.congig

--- webpack.config.js
+++ webpack.config.js
@@ -27,23 +27,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins: function() {
-              return [
-                require('precss'),
-                require('autoprefixer'),
-              ];
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer'), // eslint-disable-line global-require
+              ],
             },
           },
-        }, {
-          loader: 'sass-loader',
-        }],
+        ],
       },
     ],
   },
